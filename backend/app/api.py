dfda609253df61fb9c300d65b0332eb1d2d52232
g33ya <115582758+g33ya@users.noreply.github.com>
2026-08-22 20:36:21 -0400

    Connecting backend to database

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from backend.database import get_connection
+
 
 app = FastAPI()
 
@@ -22,3 +24,12 @@ app.add_middleware(
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "FastAPI is running!"}
+
+@app.get("/trips")
+def get_trips():
+    with get_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT id, name FROM trips")
+            trips = cursor.fetchall()
+
+    return [{"id": trip[0], "name": trip[1]} for trip in trips]
