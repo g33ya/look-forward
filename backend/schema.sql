7906bdc1ad5f03fae383b470864c1630497a6c91
g33ya <115582758+g33ya@users.noreply.github.com>
2026-08-23 12:19:42 -0400

    Created activities table (SQL)

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,4 +1,17 @@
+DROP TABLE trips CASCADE;
+DROP TABLE activities CASCADE;
+
 CREATE TABLE trips (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE activities (
+    id SERIAL PRIMARY KEY,
+    trip_id INT REFERENCES trips(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    price_range VARCHAR(255) CHECK (price_range IN ('$', '$$', '$$$')),
+    links VARCHAR(2048),
+    notes TEXT
+)
